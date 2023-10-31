0b4102b842283518a8e819ec01145818e33fb99b
Petter Moe Kvalvaag <petter@ascenium.com>
2023-10-31 21:06:45 +0100

    Add spacemacs files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Setup VSCode
 code --install-extension vscodevim.vim
+
+mkdir -p ~/Code
+
+if [ ! -d "$HOME/.emacs.d" ]; then
+  git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cp "$SCRIPT_DIR/.spacemacs" ~/.spacemacs
 
