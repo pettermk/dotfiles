c6a9a1e67cc895342746cbf7e1534f83fd778593
Petter Kvalvaag <petter.kvalvaag@proton.me>
2025-01-09 17:31:23 +0100

    Changed neovim config to bootstrapped by NvChad

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Setup VSCode
-code --install-extension vscodevim.vim
-
-mkdir -p ~/Code
-
-if [ ! -d "$HOME/.emacs.d" ]; then
-  git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-fi
-
+# code --install-extension vscodevim.vim
+# 
+# mkdir -p ~/Code
+# 
+# if [ ! -d "$HOME/.emacs.d" ]; then
+#   git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+# fi
+# 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cp "$SCRIPT_DIR/.spacemacs" ~/.spacemacs
+# cp "$SCRIPT_DIR/.spacemacs" ~/.spacemacs
 
 cp -R "$SCRIPT_DIR/neovim/user/" ~/.config/nvim/
 
